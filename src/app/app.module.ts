6d50f7f03dda8971cabf008d2521f1892709d849
alexis-almanza <alexis.edao@gmail.com>
2019-02-09 15:39:53 -0600

    componente nuevo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HsbcComponent } from './hsbc/hsbc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HsbcComponent
   ],
   imports: [
     BrowserModule
